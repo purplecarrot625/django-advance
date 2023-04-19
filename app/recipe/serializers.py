d2757dccec19db5d907ebc35b29cf07b44bbb497
purplecarrot625 <97133994+purplecarrot625@users.noreply.github.com>
2023-04-19 16:48:30 -0700

    Implemented update recipe tags feature

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -15,15 +15,12 @@ class RecipeSerializer(serializers.ModelSerializer):
     """Serializer for recipes"""
     tags = TagSerializer(many=True, required=False)  # many=True, because we are serializing a list of objects
     class Meta:
-        #  set the model
+        #  Set the model
         model = Recipe
         fields = ['id', 'title', 'time_minutes', 'price', 'link', 'tags']
         read_only_fields = ['id']
-
-    def create(self, validated_data):
-        """Create a recipe"""
-        tags = validated_data.pop('tags', [])
-        recipe = Recipe.objects.create(**validated_data)
+    def _get_or_create_tags(self, tags, recipe):
+        """Handle getting or creating tag as needed"""
         auth_user = self.context['request'].user
         for tag in tags:
             tag_obj, created = Tag.objects.get_or_create(
@@ -31,7 +28,30 @@ class RecipeSerializer(serializers.ModelSerializer):
                 **tag,
             )
             recipe.tags.add(tag_obj)
+
+    def create(self, validated_data):
+        """Create a recipe"""
+        tags = validated_data.pop('tags', [])
+        recipe = Recipe.objects.create(**validated_data)
+        self._get_or_create_tags(tags, recipe)
         return recipe
+
+    def update(self, instance, validated_data):
+        """Update a recipe"""
+        tags = validated_data.pop('tags', None)
+
+        # If tag is an empty list, clear all the tags
+        if tags is not None:
+            instance.tags.clear()
+            self._get_or_create_tags(tags, instance)
+
+        # Rest of the data (except tags), reassign to the instance
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance
+
 
 
 class RecipeDetailSerializer(RecipeSerializer):
