64baa7c2079b8e9dbe5087a2ba559bd8a3bf9cce
purplecarrot625 <97133994+purplecarrot625@users.noreply.github.com>
2023-05-23 17:28:57 -0700

    Implemented recipe filter feature

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,6 +2,7 @@
 Views for the recipe api
 """
 
+from drf_spectacular.utils import extend_schema, OpenApiParameter, OpenApiExample, OpenApiTypes
 from rest_framework import viewsets, mixins, status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
@@ -18,9 +19,26 @@ class RecipeViewSet(viewsets.ModelViewSet):
     authentication_classes = [TokenAuthentication,]
     permission_classes = [IsAuthenticated,]
 
+    def _params_to_ints(self, qs):
+        """ Convert a list of strings to integers"""
+        return [int(str_id) for str_id in qs.split(',')]
+
     def get_queryset(self):
         """Retrieve the recipes for the authenticated user."""
-        return self.queryset.filter(user=self.request.user).order_by('-id')
+        #return self.queryset.filter(user=self.request.user).order_by('-id')
+        tags = self.queryset.query_params.get('tags')
+        ingredients = self.request.query_params.get('ingredients')
+        queryset = self.queryset
+        if tags:
+            tag_ids = self._params_to_ints(tags)
+            queryset = queryset.filter(tags__id__in=tag_ids)
+        if ingredients:
+            ingredient_ids = self._params_to_ints(ingredients)
+            queryset = queryset.filter(ingredients__id__in=ingredient_ids)
+
+        return queryset.filter(
+            user = self.request.user
+        ).order_by('-id').distinct()
 
     def get_serializer_class(self):
         """Return the serilizer class for requests."""
